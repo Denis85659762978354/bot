08cfaad50a0286a2dd0f9f93f7271a226aef981a
Denis Dubrovin <sivik.dragon@gmail.com>
2023-04-18 19:20:38 +0200

    Currency Converter

--- bot.py
+++ bot.py
@@ -1,33 +1,85 @@
+
 import telebot
+from currency_converter import  CurrencyConverter
 from telebot import types
 
 bot = telebot.TeleBot('5994112406:AAHIWj1rdeSiVSijLxuMMwpQKzy1UwZaJEg')
+currency =  CurrencyConverter()
+amount = 0
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    mess = f'Hello, <b>{message.from_user.first_name} {message.from_user.last_name}</b>'
+    mess = f'Hello, <b>{message.from_user.first_name} </b>, Enter amount:'
     bot.send_message(message.chat.id, mess, parse_mode='html')
+    bot.register_next_step_handler(message, summa)
 
-@bot.message_handler(content_types=['text'])
-def get_user_text(message):
-    if message.text == 'Hello':
-        bot.send_message(message.chat.id, "Hello!", parse_mode='html')
-    elif message.text == 'id':
-        bot.send_message(message.chat.id, f"Your ID: {message.from_user.id}", parse_mode='html')
-    elif message.text == 'photo':
-        photo = open('photo1.jpg', 'rb')
-        bot.send_photo(message.chat.id, photo)
+@bot.message_handler(commands=['help'])
+def website(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard = True, row_width = 1)
+    website = types.KeyboardButton('/website')
+    start = types.KeyboardButton('/start')
+    help = types.KeyboardButton('/help')
+    markup.add(website, start, help)
+    bot.send_message(message.chat.id, 'You can go to site /website', reply_markup=markup)
+
+
+def summa(message):
+    global amount
+    try:
+        amount = int(message.text.strip())
+    except ValueError:
+        bot.send_message(message.chat.id, 'Invalid fotmat enter amount')
+        bot.register_next_step_handler(message, summa)
+        return
+
+    if amount > 0:
+        markup = types.InlineKeyboardMarkup(row_width = 2)
+        b1 = types.InlineKeyboardButton('USD/EUR', callback_data='usd/eur')
+        b2 = types.InlineKeyboardButton('EUR/USD', callback_data='eur/usd')
+        b3 = types.InlineKeyboardButton('PLN/USD', callback_data='pln/usd')
+        b4 = types.InlineKeyboardButton('GBP/USD', callback_data='gbp/usd')
+        b5 = types.InlineKeyboardButton('USD/PLN', callback_data='usd/pln')
+        b6 = types.InlineKeyboardButton('Another', callback_data='else')
+        markup.add(b1, b2, b3, b4, b5, b6)
+        bot.send_message(message.chat.id, 'Select currency pair, If you need help click : /help ', reply_markup = markup)
     else:
-        bot.send_message(message.chat.id, "I don't understand you", parse_mode='html')
+        bot.send_message(message.chat.id, 'Number must be greated then 0')
+        bot.register_next_step_handler(message, summa)
 
-@bot.message_handler(content_types=['photo'])
-def get_user_photo(message):
-    bot.send_message(message.chat.id, 'Good photo')
+@bot.callback_query_handler(func=lambda call: True)
+def callback(call):
+    if call.data != 'else':
+        values = call.data.upper().split('/')
+        res = currency.convert(amount, values[0], values[1])
+        bot.send_message(call.message.chat.id, f'Come out: {round(res, 2)}. You can re-enter the amount')
+        bot.register_next_step_handler(call.message, summa)
+    else:
+        if call.data == 'help':
+            website(call.message)
+        else:
+            bot.send_message(call.message.chat.id, 'Enter a pair of values via / ')
+            bot.register_next_step_handler(call.message, my_currency)
+
+def my_currency(message):
+    try:
+        values = message.text.upper().split('/')
+        res = currency.convert(amount, values[0], values[1])
+        bot.send_message(message.chat.id, f'Come out: {round(res, 2)}. You can re-enter the amount')
+        bot.register_next_step_handler(message, summa)
+    except Exception:
+        bot.send_message(message.chat.id, f'Something is wrong, enter the meaning again')
+        bot.register_next_step_handler(message, my_currency)
 
 @bot.message_handler(commands=['website'])
 def website(message):
     markup = types.InlineKeyboardMarkup()
-    markup.add(types.InlineKeyboardButton('Go to Website', url="https://www.pythonanywhere.com"))
-    bot.send_message(message.chat.id, 'Go to site', reply_markup=markup)
+    markup.add(types.InlineKeyboardButton('Go to Website', url="https://www.xe.com/currencyconverter"))
+    bot.send_message(message.chat.id, 'Currency Converter', reply_markup=markup)
+
+
+@bot.message_handler(content_types=['text'])
+def get_user_text(message):
+        bot.send_message(message.chat.id, "I don't understand you", parse_mode='html')
+
 
 bot.polling(none_stop=True)
